368e525c1d87b4fe33d1dea1f6d02b7a11088df6
wangx <xyoriginal@126.com>
2023-01-31 01:08:27 +0800

    init

---
+++ src/main/java/com/wangx/interface_stock/InterfaceStockApplication.java
@@ -0,0 +1,13 @@
+package com.wangx.interface_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterfaceStockApplication.class, args);
+    }
+
+}
